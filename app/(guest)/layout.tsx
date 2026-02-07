7377522523ea6c84e560b4c566ab4fbe5e40f382
Ian Mark J. Buenaflor <ianmarkbuenaflor9@gmail.com>
2026-02-07 12:11:18 +0800

    fixing some issues in layout

--- app/(guest)/layout.tsx
+++ app/(guest)/layout.tsx
@@ -1,3 +1,7 @@
-export default function Projects() {
-  return <div>Project Page</div>;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main>{children}</main>;
 }
